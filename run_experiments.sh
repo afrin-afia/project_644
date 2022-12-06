dec2e0a420076b08a87a5b8d88529b22e5964917
J27avier <salesortiz@gmail.com>
2022-12-06 01:45:40 -0700

    Minor modification

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#python3 flower_args.py -R 0 -Y [Done]
-python3 flower_args.py -R 1 -Y 
-python3 flower_args.py -R 3 -Y 
-python3 flower_args.py -R 10 -Y 
-python3 flower_args.py -R 30 -Y 
-python3 flower_args.py -R 100 -Y 
-python3 flower_args.py -R 300 -Y 
-python3 flower_args.py -R 1000 -Y 
-python3 flower_args.py -R 3000 -Y 
-python3 flower_args.py -R 10000 -Y 
+#python3 flower_args.py -R 0 [Done]
+python3 flower_args.py -R 1  
+python3 flower_args.py -R 3  
+python3 flower_args.py -R 10  
+python3 flower_args.py -R 30  
+python3 flower_args.py -R 100  
+python3 flower_args.py -R 300  
+python3 flower_args.py -R 1000  
+python3 flower_args.py -R 3000  
+python3 flower_args.py -R 10000  
